b6b57bc7f549c366fe1ef21111ebcdb8665b01fb
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-01-25 12:12:08 -0500

    Completed

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  let isNumber = (currentValue) => typeof currentValue === 'number';
+  
+  let result = rolls.every(isNumber);
+
+  return result;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +17,20 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  if(rolls.length === 0){
+      return null;
+  }
+  
+  for(element of rolls){
+      if(element === value){
+          return element;
+          break;
+      }
+  }
+  
+  return null;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +38,40 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let array = [];
+  
+  if(rolls.length === 0){
+      return array;
+  }
+  
+  for(let element of rolls){
+      if(element >= lowest){
+          array.push(element);
+      }
+  }
+  return array;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let obj = {};
+  
+  if(rolls.length === 0){
+      return obj;
+  }
+  
+rolls.forEach((value) => {
+    obj[value] = obj[value] || 0;
+    obj[value]++;
+})
+  
+  return obj;
+}
 
 // Do not change the code below here.
 module.exports = {
