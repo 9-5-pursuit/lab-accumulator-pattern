70c68502af9fefab7c8455c75bb7806ac72c5685
Michelle Harley <chell.hx@gmail.com>
2023-01-25 11:57:18 -0500

    completed AP lab

--- index.js
+++ index.js
@@ -3,7 +3,22 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+
+  let rollBool = false;
+
+  if (rolls.length === 0){
+    return true
+  }
+
+  for (let roll of rolls){
+  
+    if (typeof roll === "number"){
+     rollBool = true;
+    } else if (roll !== "number") rollBool = false; }
+
+  return rollBool;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +26,21 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  let valueFound = null;
+
+  for (let num of rolls){
+
+    if (num === value){
+      valueFound = num;
+      return valueFound;
+    } 
+
+  }
+
+  return valueFound;
+
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +48,36 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let newRolls = [];
+
+  for (let num of rolls){
+
+    if (num >= lowest){
+      newRolls.push(num)
+    }
+
+  }
+
+  return newRolls;
+
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let objRolls = {};
+  
+  for (let i = 0; i < rolls.length; i++){
+    objRolls[rolls[i]] = (objRolls[rolls[i]] + 1) || 1;
+  }
+
+  return objRolls;
+
+}
 
 // Do not change the code below here.
 module.exports = {
