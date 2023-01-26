552854d2fd672e177403c638b45fa9374786160e
daleskiii <aun2793@gmail.com>
2023-01-26 07:53:15 -0500

    HW-on-accumalator patter

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls){
+ if (!Array.isArray(rolls)) {
+  return []
+ } else return rolls.every(val => typeof val === 'number')
+  
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +16,19 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (let i = 0; i < rolls.length; i++) {
+    if (rolls[i] === value) {
+      return value;
+    }
+  }
+  return null;
+}
+
+   
+  
+
+
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +36,28 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+    const lowestValue = Math.min(...rolls);
+    return rolls.filter(x => x >= lowestValue);
+}
+
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls){
+let roll = {}
+  for (const key in rolls) {
+    roll.push({
+        number: key,
+        count: rolls[key]
+  });
+}
+return roll;
+}
+  
 
 // Do not change the code below here.
 module.exports = {
